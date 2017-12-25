e8896b951e6f9f2a8494ffc3d40286deb9a9b0e9
Geoff Golder <geoff@ixisdigital.com>
2017-12-25 16:04:36 -0500

    exposing ports and adding way to get config

---
+++ namecoind-docker/getconfig.sh
@@ -0,0 +1,1 @@
+sudo docker exec -it namecoin cat /root/.namecoin/namecoin.conf
--- namecoind-docker/start.sh
+++ namecoind-docker/start.sh
@@ -1,1 +1,1 @@
-sudo docker run -d --name namecoin -v /srv/docker/namecoin:/namecoin_data namecoin namecoin
+sudo docker run -d --name namecoin -v /srv/docker/namecoin:/namecoin_data -p 8336:8336 namecoin namecoin
