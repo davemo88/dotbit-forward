91fb41d6579b639a7e4689944ad7fb4a5bd21f77
Geoff Golder <geoff@ixisdigital.com>
2017-12-25 12:37:40 -0500

    namecoin docker

---
+++ namecoind-docker/docker-entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+if [ "$1" = 'namecoin' ]; then
+    exec /data/namecoin/bin/namecoind -datadir=/data -conf=/root/.namecoin/namecoin.conf `echo ${@:2}`
+else
+    exec "$@"
+fi
