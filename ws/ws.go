41bba666c0ed6d943814926654a49688faf85c0b
dk <davidkasofsky@gmail.com>
2017-12-26 00:04:37 -0500

    go

--- ws/ws.go
+++ ws/ws.go
@@ -19,10 +19,36 @@ type rpcResponse struct {
   Error         string `json:"error"`
 }
 
-func DotBitForward(w http.ResponseWriter, r *http.Request) {
-//  domainParts := strings.Split(r.Host, ".")
-  domainParts := [2]string{"hg", "taco"}
-  jsonStr := fmt.Sprintf(`{"jsonrpc":"1.0","id":"gotext","method":"name_filter","params":["^d/%v$"]}`, domainParts[0])
+func hasKey(m map[string]string, s string) bool {
+  _, ok := m[s]
+  return ok
+}
+
+func getIpFromMap(mapVal string, subdomain string) string {
+  return "bogus"
+}
+
+func getDotBitDomain(requestHost string) (string, string) {
+  return "", "hg"
+}
+
+//func getRedirectIp() {
+//  var redirectIp string
+//
+//  switch {
+//  case hasKey(dotBitRecord, "ip"):
+//    redirectIp = dotBitRecord["ip"]
+//  case hasKey(dotBitRecord, "map"):
+//    redirectIp = getIpFromMap(dotBitRecord["map"],dotBitSubdomain)
+//  case hasKey(dotBitRecord, "ns"):
+//  }
+//
+//  ftm.Fprintln(w, redirectIp)
+//}
+
+func dotBitForward(w http.ResponseWriter, r *http.Request) {
+  dotBitSubdomain, dotBitDomain := getDotBitDomain(r.Host)
+  jsonStr := fmt.Sprintf(`{"jsonrpc":"1.0","id":"gotext","method":"name_filter","params":["^d/%v$"]}`, dotBitDomain)
   req, err := http.NewRequest("POST", NAMECOIND_HOST, bytes.NewBuffer([]byte(jsonStr)))
   req.Header.Add("content-type","text/plain")
   req.SetBasicAuth("rpcuser","tacos")
@@ -33,14 +59,29 @@ func DotBitForward(w http.ResponseWriter, r *http.Request) {
     panic(err)
   }
   body, _ := ioutil.ReadAll(resp.Body)
-  fmt.Fprintln(w, string(body))
+//  fmt.Fprintln(w, string(body))
   var data rpcResponse
   _ = json.Unmarshal(body, &data)
-  fmt.Fprintln(w, data.Result[0]["value"])
-//  http.Redirect(w,r,"http://google.com",301)
+//  fmt.Fprintln(w, data.Result[0]["value"])
+
+  var dotBitRecord map[string]string
+  _ = json.Unmarshal([]byte(data.Result[0]["value"]), &dotBitRecord)
+
+  var redirectIp string
+
+  switch {
+  case hasKey(dotBitRecord, "ip"):
+    redirectIp = dotBitRecord["ip"]
+  case hasKey(dotBitRecord, "map"):
+    redirectIp = getIpFromMap(dotBitRecord["map"],dotBitSubdomain)
+  case hasKey(dotBitRecord, "ns"):
+  }
+
+  fmt.Fprintln(w, redirectIp)
+
 }
 
 func main() {
-    http.HandleFunc("/", DotBitForward)
+    http.HandleFunc("/", dotBitForward)
     http.ListenAndServe(":8080", nil)
 }
