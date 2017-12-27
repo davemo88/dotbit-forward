75209a9a64994a2273e8735a622083d7cc2cf327
dk <davidkasofsky@gmail.com>
2017-12-27 00:43:41 -0500

    got an ip

--- ws/ws.go
+++ ws/ws.go
@@ -11,75 +11,86 @@ import (
 
 const NAMECOIND_HOST = "http://127.0.0.1:8336"
 
-type nameValue map[string]string
+type stringMap map[string]string
 
 type rpcResponse struct {
-  Result        []nameValue `json:"result"`
+  Result        []stringMap `json:"result"`
   Id            string `json:"id"`
   Error         string `json:"error"`
 }
 
-func hasKey(m map[string]string, s string) bool {
-  _, ok := m[s]
-  return ok
-}
-
-func getIpFromMap(mapVal string, subdomain string) string {
-  return "bogus"
-}
-
-func getDotBitDomain(requestHost string) (string, string) {
-  return "", "hg"
-}
-
-//func getRedirectIp() {
-//  var redirectIp string
-//
-//  switch {
-//  case hasKey(dotBitRecord, "ip"):
-//    redirectIp = dotBitRecord["ip"]
-//  case hasKey(dotBitRecord, "map"):
-//    redirectIp = getIpFromMap(dotBitRecord["map"],dotBitSubdomain)
-//  case hasKey(dotBitRecord, "ns"):
-//  }
-//
-//  ftm.Fprintln(w, redirectIp)
-//}
-
 func dotBitForward(w http.ResponseWriter, r *http.Request) {
   dotBitSubdomain, dotBitDomain := getDotBitDomain(r.Host)
-  jsonStr := fmt.Sprintf(`{"jsonrpc":"1.0","id":"gotext","method":"name_filter","params":["^d/%v$"]}`, dotBitDomain)
-  req, err := http.NewRequest("POST", NAMECOIND_HOST, bytes.NewBuffer([]byte(jsonStr)))
-  req.Header.Add("content-type","text/plain")
-  req.SetBasicAuth("rpcuser","tacos")
-
+  req := getRpcRequest(dotBitDomain)
   client := &http.Client{}
   resp, err := client.Do(req)
   if err != nil {
     panic(err)
   }
+  dotBitRecord := getDotBitRecord(resp)
+  redirectIp := getRedirectIp(dotBitRecord, dotBitSubdomain)
+  http.Redirect(w, r, redirectIp, 303)
+}
+
+func getDotBitDomain(requestHost string) (string, string) {
+  return "", "2ez"
+}
+
+func getRpcRequest(dotBitDomain string) *http.Request {
+  jsonStr := fmt.Sprintf(`{"jsonrpc":"1.0","id":"gotext","method":"name_filter","params":["^d/%v$"]}`, dotBitDomain)
+  req, _ := http.NewRequest("POST", NAMECOIND_HOST, bytes.NewBuffer([]byte(jsonStr)))
+  req.Header.Add("content-type","text/plain")
+  req.SetBasicAuth("rpcuser","tacos")
+  return req
+}
+
+func getDotBitRecord(resp *http.Response) stringMap{
   body, _ := ioutil.ReadAll(resp.Body)
 //  fmt.Fprintln(w, string(body))
   var data rpcResponse
   _ = json.Unmarshal(body, &data)
 //  fmt.Fprintln(w, data.Result[0]["value"])
 
-  var dotBitRecord map[string]string
+  var dotBitRecord stringMap
+// why is Result an array
   _ = json.Unmarshal([]byte(data.Result[0]["value"]), &dotBitRecord)
 
+  return dotBitRecord
+}
+
+func getRedirectIp(dotBitRecord stringMap, dotBitSubdomain string) string {
   var redirectIp string
 
   switch {
-  case hasKey(dotBitRecord, "ip"):
+  case dotBitSubdomain == "" && hasKey(dotBitRecord, "ip"):
     redirectIp = dotBitRecord["ip"]
-  case hasKey(dotBitRecord, "map"):
-    redirectIp = getIpFromMap(dotBitRecord["map"],dotBitSubdomain)
+  case dotBitSubdomain == "" && hasKey(dotBitRecord, "map"):
+    redirectIp = getIpFromMap(dotBitRecord["map"], dotBitSubdomain)
   case hasKey(dotBitRecord, "ns"):
+//    nameservers := dotBitRecord["ns"]
+  case hasKey(dotBitRecord, "translate"):
   }
 
-  fmt.Fprintln(w, redirectIp)
-
+  return redirectIp
 }
+
+func getIpFromMap(mapVal string, subdomain string) string {
+  var m stringMap
+  _ = json.Unmarshal([]byte(mapVal), &m)
+  var ip string
+  if hasKey(m, subdomain) {
+    ip = m[subdomain]
+  } else {
+    ip = ""
+  }
+  return ip
+}
+
+func hasKey(m stringMap, s string) bool {
+  _, ok := m[s]
+  return ok
+}
+
 
 func main() {
     http.HandleFunc("/", dotBitForward)
